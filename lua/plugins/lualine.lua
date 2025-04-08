e571b14537279b9b54bc9d90971d3961e544e258
Chris Petkau <47264230+chrispetkau@users.noreply.github.com>
2025-04-08 06:22:53 -0700

    Lualine. Better display of filename.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,25 +1,52 @@
 local lualine = {}
 
 local function dap_status()
-    local session = require("dap").session()
-    -- return session and " DAP Connected" or " DAP Disconnected"
-    return session and "" or ""
+	local session = require("dap").session()
+	-- return session and " DAP Connected" or " DAP Disconnected"
+	return session and "" or ""
 end
 
 function lualine.setup()
 	require('lualine').setup({
-		extensions = { 'fugitive', 'nvim-dap-ui' },
-		sections = {
-			lualine_x = { dap_status, 'filetype', },
+		options = {
+			icons_enabled = true,
+			theme = 'auto',
+			component_separators = { left = '', right = ''},
+			section_separators = { left = '', right = ''},
+			disabled_filetypes = {
+				statusline = {},
+				winbar = {},
+			},
+			ignore_focus = {},
+			always_divide_middle = true,
+			always_show_tabline = true,
+			globalstatus = false,
+			refresh = {
+				statusline = 100,
+				tabline = 100,
+				winbar = 100,
+			}
 		},
-		-- tabline = {
-		-- 	lualine_a = {},
-		-- 	lualine_b = {'branch'},
-		-- 	lualine_c = {'filename'},
-		-- 	lualine_x = {},
-		-- 	lualine_y = {},
-		-- 	lualine_z = {'tabs'},
-		-- },
+		sections = {
+			lualine_a = {'mode'},
+			lualine_b = {'branch', 'diff', 'diagnostics'},
+			lualine_c = {{'filename', path = 1}},
+			lualine_x = {dap_status, 'lsp_status'},
+			lualine_y = {'filetype'},
+			lualine_z = {'progress', 'location'}
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = {'filename'},
+			lualine_x = {'location'},
+			lualine_y = {},
+			lualine_z = {}
+		},
+		tabline = {},
+		winbar = {},
+		inactive_winbar = {},
+		extensions = { 'fugitive', 'nvim-dap-ui' },
 	})
 end
 
