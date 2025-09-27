26c0bf1a80952555714dfa12819bac3d3ed5bc23
Chris Petkau <47264230+chrispetkau@users.noreply.github.com>
2025-09-27 08:40:50 -0700

    No snippets in auto-complete, please. LSP-sanctioned stuff only!

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -44,7 +44,7 @@ function mod.setup()
 			{ name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
 			-- Remove buffer source to suppress Text options.
 			-- { name = 'buffer', keyword_length = 2 },        -- source current buffer
-			{ name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
+			-- { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
 			{ name = 'calc'},                               -- source for math calculation
 		},
 		window = {
